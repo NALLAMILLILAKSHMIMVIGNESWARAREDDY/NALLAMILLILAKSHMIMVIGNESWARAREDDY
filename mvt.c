41d124ba08fe424baf32d61e78d59d75414d7a79
NALLAMILLILAKSHMIMVIGNESWARAREDDY <98519503+NALLAMILLILAKSHMIMVIGNESWARAREDDY@users.noreply.github.com>
2022-12-02 15:02:45 +0530

    Update mvt.c

--- mvt.c
+++ mvt.c
@@ -30,3 +30,38 @@ printf("\n \t%d\t\t%d",i+1,mp[i]);
 printf("\n\nTotal Memory Allocated is %d",ms-temp);
 printf("\nTotal External Fragmentation is %d",temp);
 }
+/*Enter the total memory available (in Bytes)--1000
+
+Enter memory required for process 1 (in Bytes)--128
+
+memory is allocated for process 1
+Do you want to continue(y/n)--y
+
+Enter memory required for process 2 (in Bytes)--256
+
+memory is allocated for process 2
+Do you want to continue(y/n)--y
+
+Enter memory required for process 3 (in Bytes)--64
+
+memory is allocated for process 3
+Do you want to continue(y/n)--y
+
+Enter memory required for process 4 (in Bytes)--512
+
+memory is allocated for process 4
+Do you want to continue(y/n)--y
+
+Enter memory required for process 5 (in Bytes)--1024
+
+Memory is full
+
+Total Memory available-- 1000
+
+        PROCESS          MEMORY ALLOCATED
+        1               128
+        2               256
+        3               64
+        4               512
+
+*/
