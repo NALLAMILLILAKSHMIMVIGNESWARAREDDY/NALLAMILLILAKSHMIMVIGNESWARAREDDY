f59a4b8fcb04fc8590b25831726f23b1612ae926
NALLAMILLILAKSHMIMVIGNESWARAREDDY <98519503+NALLAMILLILAKSHMIMVIGNESWARAREDDY@users.noreply.github.com>
2022-12-02 14:39:59 +0530

    Create mvtoutput.c

---
+++ mvtoutput.c
@@ -0,0 +1,36 @@
+Enter the total memory available (in Bytes)--1000
+
+Enter memory required for process 1 (in Bytes)--128
+
+memory is allocated for process 1
+Do you want to continue(y/n)--y
+
+Enter memory required for process 2 (in Bytes)--256
+
+memory is allocated for process 2
+Do you want to continue(y/n)--y
+
+Enter memory required for process 3 (in Bytes)--64
+
+memory is allocated for process 3
+Do you want to continue(y/n)--y
+
+Enter memory required for process 4 (in Bytes)--512
+
+memory is allocated for process 4
+Do you want to continue(y/n)--y
+
+Enter memory required for process 5 (in Bytes)--1024
+
+Memory is full
+
+Total Memory available-- 1000
+
+        PROCESS          MEMORY ALLOCATED
+        1               128
+        2               256
+        3               64
+        4               512
+
+Total Memory Allocated is 960
+Total External Fragmentation is 40
