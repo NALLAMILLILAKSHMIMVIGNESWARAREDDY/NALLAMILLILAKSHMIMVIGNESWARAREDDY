176a3da32bd2780f077704c541582a0a85444143
NALLAMILLILAKSHMIMVIGNESWARAREDDY <98519503+NALLAMILLILAKSHMIMVIGNESWARAREDDY@users.noreply.github.com>
2022-12-02 15:10:10 +0530

    Update fifo.c

--- fifo.c
+++ fifo.c
@@ -39,3 +39,27 @@ printf("\n");
 printf("Page Fault is %d",fcount);
 return 0;
 }
+/*
+ ENTER THE NUMBER OF PAGES:
+7
+
+ ENTER THE PAGE NUMBER :
+24
+43
+55
+65
+76
+76
+45
+
+ ENTER THE NUMBER OF FRAMES :5
+
+ ref string      page frames    Hit/fault
+24              24      -1      -1      -1      -1      F
+43              24      43      -1      -1      -1      F
+55              24      43      55      -1      -1      F
+65              24      43      55      65      -1      F
+76              24      43      55      65      76      F
+76              24      43      55      65      76      H
+45              45      43      55      65      76      F
+Page Fault is 6*/
